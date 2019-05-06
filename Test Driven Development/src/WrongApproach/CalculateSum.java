9cdb5aa47f25f5a4b17d441a331ce02169a21b8c
navne <navne@DESKTOP-J6LIU6M>
2019-05-06 15:36:58 -0300

    adding wrong approach method to function

---
+++ Test Driven Development/src/WrongApproach/CalculateSum.java
@@ -0,0 +1,11 @@
+package WrongApproach;
+
+public class CalculateSum {
+	public int Sum(int firstValue, int secondValue) {
+		if(firstValue!=' ' && secondValue != ' ') {
+			return firstValue + secondValue;
+		}else {
+			return 0;
+		}
+	}
+}
