e4ed73522f5eac1028d1a2cf62c956e584f96c2b
NavneetPrakashSingh <navneet_prakash_singh@live.com>
2019-05-06 18:52:29 -0300

    Adding test and mock objects

--- Test Driven Development/src/Test/MockSum.java
+++ Test Driven Development/src/Test/MockSum.java
@@ -1,5 +1,38 @@
 package Test;
+import Main.ModelSum;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MockSum {
-
+	private Map<Integer, ModelSum> modelSumMap;
+	public MockSum() {
+		modelSumMap = new HashMap<Integer, ModelSum>();
+		mockObjects();
+	}
+	public void mockObjects() {
+		ModelSum firstObject = new ModelSum();
+		firstObject.setFirstValue(-1);
+		firstObject.setSecondValue(1);
+		modelSumMap.put(0, firstObject);
+		
+		ModelSum secondObject = new ModelSum();
+		secondObject.setFirstValue(0);
+		secondObject.setSecondValue(0);
+		modelSumMap.put(1, firstObject);
+		
+		ModelSum thirdObject = new ModelSum();
+		thirdObject.setFirstValue(1);
+		thirdObject.setSecondValue(1);
+		modelSumMap.put(2, firstObject);
+		
+		ModelSum fourthObject = new ModelSum();
+		fourthObject.setFirstValue(-1);
+		fourthObject.setSecondValue(-1);
+		modelSumMap.put(3, firstObject);
+	}
+	
+	public ModelSum loadObject(int passedIndexOfMap) {
+		return modelSumMap.get(passedIndexOfMap);
+	}
 }
--- Test Driven Development/src/Test/TestSum.java
+++ Test Driven Development/src/Test/TestSum.java
@@ -1,5 +1,40 @@
 package Test;
 
-public class TestSum {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class TestSum {
+	
+	static MockSum mockSum; 
+	@BeforeAll
+	static void setUpBeforeClass()
+	{
+		mockSum = new MockSum();
+	}
+	
+	@Test
+	void TestForPositiveNegativeSum() {
+		int sum = mockSum.loadObject(0).getFirstValue() + mockSum.loadObject(0).getSecondValue();
+		assertEquals(sum, 0);
+	}
+	
+	@Test
+	void TestForZeroSum() {
+		int sum = mockSum.loadObject(1).getFirstValue() + mockSum.loadObject(1).getSecondValue();
+		assertEquals(sum, 0);
+	}
+	
+	@Test
+	void TestForPositiveSum() {
+		int sum = mockSum.loadObject(2).getFirstValue() + mockSum.loadObject(2).getSecondValue();
+		assertEquals(sum, 0);
+	}
+	
+	@Test
+	void TestForNegativeSum() {
+		int sum = mockSum.loadObject(3).getFirstValue() + mockSum.loadObject(3).getSecondValue();
+		assertEquals(sum, 0);
+	}
 }
